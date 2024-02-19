ce920b6c94776cf1f157fa56e200ec30397e9255
SaraQ2030 <159685978+SaraQ2030@users.noreply.github.com>
2024-02-19 11:40:07 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,89 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input=new Scanner(System.in);
+        // Q1-----------------------------------------------------------------------------------------------------------
+////
+//        System.out.println("Enter the first number :");
+//        int num1=input.nextInt();
+//        System.out.println("Enter the second number :");
+//        int num2=input.nextInt();
+//        System.out.println("Enter the third number :");
+//        int num3=input.nextInt();
+//
+//        if (num1>num2){
+//            if(num1>num3){
+//                System.out.println("The largest number is :"+num1);
+//            }else    System.out.println("EThe largest number is :"+num3);
+//        } else if (num2>num3) {
+//            System.out.println("EThe largest number is :"+num2);
+//
+//        }else {System.out.println("EThe largest number is :"+num3);}
+//
+
+
+        //Q2------------------------------------------------------------------------------------------------------------
+//       System.out.println("Enter the string :");
+//        String word=input.nextLine();
+//        System.out.println("Enter the number of index :");
+//        int n=input.nextInt();
+//        if(n>word.length()){
+//            System.out.println("The number is out of String boundaries ");
+//        }else {
+//            System.out.println("The charcter is  :" + word.substring(n, n + 1));
+//      }
+
+        //Q3------------------------------------------------------------------------------------------------------------
+
+//        int sum=0;
+//        int n;
+//        do {
+//            System.out.println("Enter the number to add or 0 to quit :");
+//            n=input.nextInt();
+//            sum+=n;
+//
+//        }while (n!=0);
+//        System.out.println("The total sum is "+sum);
+
+        //Q4------------------------------------------------------------------------------------------------------------
+//        ArrayList<Integer> arr=new ArrayList<Integer>();
+//        arr.add(10);
+//        arr.add(-21);
+//        arr.add(30);
+//        arr.add(31);
+//        arr.add(-25);
+//        for (int a:arr){
+//            if (a>0){
+//                System.out.println(a+" is Positive number ");
+//            } else if (a<0) {
+//                System.out.println(a+" is Negative number ");
+//            }else  System.out.println(a+" is Zero");
+//        }
+
+        //Q5------------------------------------------------------------------------------------------------------------
+//        String [] arr={"Tuwaiq","Bootcamp","Student","JAVA"};
+//        String word;
+//        int temp=6;
+//
+//        for (int  a=0;a<arr.length;a++){
+//
+//           word=arr[a];
+//            int count = 0;
+//           for (int i=0;i< word.length();i++) {
+//               count++;
+//
+//           } if(temp>count){
+//                temp=count;
+//                System.out.println( word);
+//            }
+//        }
+//
+
+        }
+    }
